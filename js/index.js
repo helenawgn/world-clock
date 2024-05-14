4d4fdc82eb6c9f3637a1a10d26adb161bb094d60
helenawgn <149970908+helenawgn@users.noreply.github.com>
2024-05-14 09:27:43 +0200

    added function updateTime in JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let kyivElement = document.querySelector("#kyiv");
 
+  let kyivDateElement = kyivElement.querySelector(".date");
+  let kyivTimeElement = kyivElement.querySelector(".time");
+  let kyivTime = moment().tz("Europe/Kiev");
+
+  kyivDateElement.innerHTML = kyivTime.format("MMMM	Do YYYY");
+  kyivTimeElement.innerHTML = kyivTime.format("h:mm:ss [<small>]A[</small>]");
+
+  let singaporeElement = document.querySelector("#singapore");
+
+  let singaporeDateElement = singaporeElement.querySelector(".date");
+  let singaporeTimeElement = singaporeElement.querySelector(".time");
+  let singaporeTime = moment().tz("Asia/Singapore");
+
+  singaporeDateElement.innerHTML = singaporeTime.format("MMMM	Do YYYY");
+  singaporeTimeElement.innerHTML = singaporeTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
