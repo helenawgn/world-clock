09f96bea96bda4687b31e19029dbabd1b08c195c
helenawgn <149970908+helenawgn@users.noreply.github.com>
2024-05-14 14:39:58 +0200

    added local time

--- js/index.js
+++ js/index.js
@@ -1,30 +1,34 @@
 function updateTime() {
   let kyivElement = document.querySelector("#kyiv");
-
-  let kyivDateElement = kyivElement.querySelector(".date");
-  let kyivTimeElement = kyivElement.querySelector(".time");
-  let kyivTime = moment().tz("Europe/Kiev");
-
-  kyivDateElement.innerHTML = kyivTime.format("MMMM	Do YYYY");
-  kyivTimeElement.innerHTML = kyivTime.format("h:mm:ss [<small>]A[</small>]");
-
   let singaporeElement = document.querySelector("#singapore");
 
-  let singaporeDateElement = singaporeElement.querySelector(".date");
-  let singaporeTimeElement = singaporeElement.querySelector(".time");
-  let singaporeTime = moment().tz("Asia/Singapore");
+  if (kyivElement && singaporeElement) {
+    let kyivDateElement = kyivElement.querySelector(".date");
+    let kyivTimeElement = kyivElement.querySelector(".time");
+    let kyivTime = moment().tz("Europe/Kiev");
 
-  singaporeDateElement.innerHTML = singaporeTime.format("MMMM	Do YYYY");
-  singaporeTimeElement.innerHTML = singaporeTime.format(
-    "h:mm:ss [<small>]A[</small>]",
-  );
+    kyivDateElement.innerHTML = kyivTime.format("MMMM Do YYYY");
+    kyivTimeElement.innerHTML = kyivTime.format("h:mm:ss [<small>]A[</small>]");
+
+    let singaporeDateElement = singaporeElement.querySelector(".date");
+    let singaporeTimeElement = singaporeElement.querySelector(".time");
+    let singaporeTime = moment().tz("Asia/Singapore");
+
+    singaporeDateElement.innerHTML = singaporeTime.format("MMMM Do YYYY");
+    singaporeTimeElement.innerHTML = singaporeTime.format(
+      "h:mm:ss [<small>]A[</small>]",
+    );
+  }
 }
-
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
+
   citiesElement.innerHTML = `
   <div class="city">
     <div>
